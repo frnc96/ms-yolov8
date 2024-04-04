9ca96833c2fec093da4b14fc4cf8e2824fb61a8e
Frencis Balla <frencis.balla@gmail.com>
2024-04-04 11:55:51 +0200

    ...

--- src/validate.py
+++ src/validate.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 # Config
-MODEL_YAML_PATH = Path("/home/frencis/D1/sbatch/runs/detect/4ch-yolov8x-coco-llvip_niicu-640/weights/best.pt")
+MODEL_YAML_PATH = Path("/home/frencis/D1/sbatch/runs/detect/yolov8x-coco-niicu-640/weights/best.pt")
 # DATA_YAML_PATH = Path("/home/frencis/D1/datasets/llvip/data.yaml")
 
 
