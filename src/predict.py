5816346ba95e89717a0ee0a535baad30415ef7e1
Frencis Balla <frencis.balla@gmail.com>
2024-05-11 14:05:22 +0200

    Added predict script

--- src/predict.py
+++ src/predict.py
@@ -1,23 +1,36 @@
 from ultralytics import YOLO
-# from src.rgbt import merge
-from pathlib import Path
+import numpy as np
+import cv2
 
 
-# Load a model
-model = YOLO(
-    model=Path("/home/frencis/D1/sbatch/runs/detect/4ch-yolov8x-niicu-640 (bicubic)/weights/best.pt"),
-    task="detect",
-    verbose=True,
-)
+# Load a pretrained YOLOv8n model
+model = YOLO('/home/frencis/D1/sbatch/runs/detect/4ch-yolov8x-niicu-640 (bicubic)/weights/best.pt')
 
-# # Compose the WxHx4 Tensor from two images
-# rgbt_tensor = merge(
-#     rgb_image_path='ultralytics/cfg/datasets/niicu/val/rgb-images/flight3_frame12721.jpg',
-#     t_image_path='ultralytics/cfg/datasets/niicu/val/t-images/flight3_frame12721.jpg',
-# )
+# Define path to video file
+source = f'/home/frencis/D1/datasets/niicu/train/images'
 
-results = model.predict(
-    # source=rgbt_tensor,
-    source='/home/frencis/D1/datasets/niicu/val/images/flight3_frame12721.tiff',
-    save=True,
-)
+# Run inference on the source
+results = model(source, stream=True)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter(f'/home/frencis/D1/sbatch/videos/4ch_training_set.avi', fourcc, 30, (2706, 1980))
+
+# Process results list
+for idx, result in enumerate(results):
+    frame = result.plot()  # TRGB-order numpy array
+
+    r = frame[:, :, 0]
+    g = frame[:, :, 1]
+    b = frame[:, :, 2]
+
+    frame = np.dstack((r, g, b))
+
+    # Write the frame into the video file
+    out.write(frame)
+
+    # if idx == 180:
+    #     break
+
+# Release the video writer
+out.release()
